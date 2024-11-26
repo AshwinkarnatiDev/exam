d239e4befa3a1382d7aa645065b5b8066648ddd2
Ashwinkarnati <ashwinkarnati2k5@gmail.com>
2024-11-26 08:16:28 +0530

    new feature

--- register.py
+++ register.py
@@ -5,4 +5,5 @@ root=Tk()
 
 Label(text="name").grid(row=1, column=0,padx=5,pady=5)
 Entry(root).grid(row=1, column=1,padx=5,pady=5)
+Label(text="description").grid(row=1, column=4,padx=5,pady=5)
 root.mainloop()
